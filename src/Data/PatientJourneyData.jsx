548383ea42fc8e35b8929ba54250e9d3f3d0ff1a
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2024-03-16 00:18:59 +0530

    completed

--- src/Data/PatientJourneyData.jsx
+++ src/Data/PatientJourneyData.jsx
@@ -4,57 +4,57 @@ export const PatientJourneyEnglishData = [
     title: "Frictionless Onboarding:",
     subtext:
       "Our platform makes it easy for patients to get started on their healthcare journey. With just a few clicks, they can access a range of health services safely and conveniently.",
-    icon: "/PatientJourney/icon1.png",
+    icon: "/PatientJourney/icon1.webp",
   },
   {
     id: 2,
     title: "Fast & Easy Assessments:",
-    icon: "/PatientJourney/icon2.png",
+    icon: "/PatientJourney/icon2.webp",
     subtext:
       "Patients can share details about their health history using a comprehensive questionnaire. This helps caregivers understand their needs quickly, so they can provide personalized care. Our user-friendly interface makes the process fast and efficient.",
   },
   {
     id: 3,
     title: "24/7 Care Connections:",
-    icon: "/PatientJourney/icon3.png",
+    icon: "/PatientJourney/icon3.webp",
     subtext:
       "Patients can connect with healthcare providers anytime, anywhere using chat, call, or video chat through our platform",
   },
   {
     id: 4,
     title: "All-in-one Healthcare Hub:",
-    icon: "/PatientJourney/icon4.png",
+    icon: "/PatientJourney/icon4.webp",
     subtext:
       "Access multiple healthcare services from your caregiver(s) in one place. From consultations to medication tracking, everything the patients need is in the app. Our pill tracker makes managing medications simple, so patients can stick to their treatment plans effortlessly.",
   },
 ];
 export const PatientJourneySwedishData = [
-    {
-        id: 1,
-        title: "Friktionsfri Onboarding:",
-        subtext:
-        "Vår plattform gör det enkelt för patienter att komma igång med sin vårdresa. Med bara några klick kan de få tillgång till en rad hälso- och sjukvårdstjänster på ett säkert och bekvämt sätt.",
-        icon: "/PatientJourney/icon1.png",
-    },
-    {
-        id: 2,
-        title: "Snabba och enkla bedömningar:",
-        icon: "/PatientJourney/icon2.png",
-        subtext:
-        "Patienter kan dela detaljer om sin hälsobakgrund med hjälp av en omfattande frågeformulär. Detta hjälper vårdgivare att snabbt förstå deras behov, så att de kan ge personlig vård. Vår användarvänliga gränssnitt gör processen snabb och effektiv.",
-    },
-    {
-        id: 3,
-        title: "24/7 Vårdanslutningar:",
-        icon: "/PatientJourney/icon3.png",
-        subtext:
-        "Patienter kan ansluta till vårdgivare när som helst, var som helst via chatt, samtal eller videosamtal genom vår plattform",
-    },
-    {
-        id: 4,
-        title: "Allt-i-ett Hälso- och sjukvårdshubb:",
-        icon: "/PatientJourney/icon4.png",
-        subtext:
-        "Få tillgång till flera hälso- och sjukvårdstjänster från din vårdgivare på ett och samma ställe. Från konsultationer till läkemedelsspårning finns allt patienterna behöver i appen. Vår pillspårare gör det enkelt att hantera mediciner, så att patienterna enkelt kan följa sina behandlingsplaner.",
-    },
+  {
+    id: 1,
+    title: "Friktionsfri Onboarding:",
+    subtext:
+      "Vår plattform gör det enkelt för patienter att komma igång med sin vårdresa. Med bara några klick kan de få tillgång till en rad hälso- och sjukvårdstjänster på ett säkert och bekvämt sätt.",
+    icon: "/PatientJourney/icon1.webp",
+  },
+  {
+    id: 2,
+    title: "Snabba och enkla bedömningar:",
+    icon: "/PatientJourney/icon2.webp",
+    subtext:
+      "Patienter kan dela detaljer om sin hälsobakgrund med hjälp av en omfattande frågeformulär. Detta hjälper vårdgivare att snabbt förstå deras behov, så att de kan ge personlig vård. Vår användarvänliga gränssnitt gör processen snabb och effektiv.",
+  },
+  {
+    id: 3,
+    title: "24/7 Vårdanslutningar:",
+    icon: "/PatientJourney/icon3.webp",
+    subtext:
+      "Patienter kan ansluta till vårdgivare när som helst, var som helst via chatt, samtal eller videosamtal genom vår plattform",
+  },
+  {
+    id: 4,
+    title: "Allt-i-ett Hälso- och sjukvårdshubb:",
+    icon: "/PatientJourney/icon4.webp",
+    subtext:
+      "Få tillgång till flera hälso- och sjukvårdstjänster från din vårdgivare på ett och samma ställe. Från konsultationer till läkemedelsspårning finns allt patienterna behöver i appen. Vår pillspårare gör det enkelt att hantera mediciner, så att patienterna enkelt kan följa sina behandlingsplaner.",
+  },
 ];
