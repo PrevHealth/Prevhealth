a002ebac29a6954cba88d02cf6a53650cf095241
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2024-03-17 17:25:21 +0530

    fixed caregiver

--- src/pages/Caregiver.jsx
+++ src/pages/Caregiver.jsx
@@ -24,36 +24,59 @@ const Caregiver = () => {
   }
 
   const handleQuestionClick = (index) => {
-   setSelectedQuestion((prevIndex) => (prevIndex === index ? null : index));
+    setSelectedQuestion((prevIndex) => (prevIndex === index ? null : index));
   };
 
   return (
-    <div id="Caregiver-journey" className="mb-10">
-      <Container>
-        <h1 className="fonta text-primary mt-10 mb-10  ">Caregiver Journey</h1>
-        <div className="flex flex-col-reverse lg:flex-row gap-10">
-          <div className="w-full flex gap-10 flex-col">
-            {data.map((item, index) => (
-              <div
-                key={index}
-                className="flex flex-col rounded-xl bg-[#F7F7FB] p-3"
-                onClick={() => handleQuestionClick(index)}
-              >
-                <span className="text-[22px] font-semibold text-primary">
-                  {item.question}
-                </span>
-                {selectedQuestion === index && (
+    <>
+      <div id="Caregiver-journey" className="mb-10">
+        <Container>
+          <h1 className="fonta text-primary mt-10 mb-10  ">
+            Caregiver Journey
+          </h1>
+          <div className="lg:flex hidden flex-col-reverse lg:flex-row gap-10">
+            <div className="w-full flex gap-10 flex-col">
+              {data.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col rounded-xl bg-[#F7F7FB] p-3"
+                  onClick={() => handleQuestionClick(index)}
+                >
+                  <span className="text-[22px] font-semibold text-primary">
+                    {item.question}
+                  </span>
+                  {selectedQuestion === index && (
+                    <span className="fontb mt-3">{item.answer}</span>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="w-full flex justify-center  items-center rounded-xl ">
+              <img src={images[selectedQuestion || 0]} className="" alt="" />
+            </div>
+          </div>
+          <div className="flex lg:hidden overflow-x-scroll ">
+            <div className="flex gap-10">
+              {data.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex w-[70vw] flex-col rounded-xl bg-[#F7F7FB] p-3"
+                >
+                  <div>
+                    <img src={item.Image} className=" object-cover" alt="" />
+                  </div>
+
+                  <span className="text-[24px] font-semibold text-primary">
+                    {item.question}
+                  </span>
                   <span className="fontb mt-3">{item.answer}</span>
-                )}
-              </div>
-            ))}
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="w-full flex justify-center  items-center rounded-xl ">
-            <img src={images[selectedQuestion || 0]} className="" alt="" />
-          </div>
-        </div>
-      </Container>
-    </div>
+        </Container>
+      </div>
+    </>
   );
 };
 
